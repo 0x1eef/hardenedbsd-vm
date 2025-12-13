1682f624837732e45a45def9a432e4db1534453c
0x1eef <0x1eef@protonmail.com>
2025-12-13 20:21:33 -0300

    Add 'set -x' to script.sh

--- internal/script/main.go
+++ internal/script/main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func Save(str string) (string, error) {
-	payload := fmt.Sprintf("#!bin/sh\n%s\n", str)
+	payload := fmt.Sprintf("#!bin/sh\nset -x\n%s\n", str)
 	return "script.sh", os.WriteFile("script.sh", []byte(payload), 0644)
 }
